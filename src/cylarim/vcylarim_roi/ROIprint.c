a561e80cd05c69b3b8ddb4feee2df5b9d1080688
Gabriele Lohmann <lohmann@tuebingen.mpg.de>
2025-06-17 10:01:12 +0200

    vcylarim_roi added

---
+++ src/cylarim/vcylarim_roi/ROIprint.c
@@ -0,0 +1,66 @@
+/*
+** cylarim: laminar statistics in a ROI
+**
+** G.Lohmann, MPI-KYB, Nov 2024
+*/
+#include <viaio/Vlib.h>
+#include <viaio/VImage.h>
+
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_errno.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+void ROIprint(VImage zmap,VImage metric,VImage roi,int nbins,VString filename)
+{
+  size_t i,j;
+  double u,v,z,jx;
+
+  double nx = (double)nbins;
+  double *s1 = (double *)VCalloc(nbins,sizeof(double));
+  double *s2 = (double *)VCalloc(nbins,sizeof(double));
+  double *kx = (double *)VCalloc(nbins,sizeof(double));
+  double *mean = (double *)VCalloc(nbins,sizeof(double));
+  double *sd = (double *)VCalloc(nbins,sizeof(double));
+
+  VFloat *pz = VImageData(zmap);
+  VFloat *pm = VImageData(metric);
+  VBit   *pr = VImageData(roi);
+  for (i=0; i<VImageNPixels(metric); i++) {
+    if (pr[i] == 0) continue;
+    u = pm[i];
+    z = pz[i];
+    if (u < TINY || u > 1.0) continue;
+    if (fabs(z) < TINY) continue;
+    for (j=1; j<=nbins; j++) {
+      jx = (double)j;
+      if (u > (jx-1.0)/nx && u <= jx/nx) {
+	s1[j-1] += z;	
+	s2[j-1] += z*z;
+	kx[j-1]++;
+      }
+    }
+  }
+ 
+  for (j=0; j<nbins; j++) {
+    if (kx[j] < 5) continue;
+    mean[j] = s1[j]/kx[j];
+    sd[j] = sqrt((s2[j] - kx[j] * mean[j] * mean[j]) / (kx[j] - 1.0));
+  }
+  
+  FILE *fp = fopen(filename,"w");
+  if (!fp) VError(" err opening %s",filename);
+
+  for (j=0; j<nbins; j++) {
+    u = (double)j/(double)nbins;
+    v = (double)(j+1)/(double)nbins;
+    fprintf(fp," %8.4f  %8.4f  %8.4f   %.2f\n",0.5*(u+v),mean[j],sd[j],kx[j]);
+  }
+
+  fclose(fp);
+}
---
+++ src/cylarim/vcylarim_roi/vcylarim_roi.c
@@ -0,0 +1,96 @@
+/*
+** Cylarim:  Cylinder-based Algorithm for Laminar fMRI Data Analysis
+**
+** G.Lohmann, MPI-KYB, Oct 2024
+*/
+
+
+/* From the standard C library: */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* From the Vista library: */
+#include <viaio/VImage.h>
+#include <viaio/Vlib.h>
+#include <viaio/mu.h>
+#include <viaio/option.h>
+
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_sort.h>
+#include <gsl/gsl_multifit.h>
+#include <gsl/gsl_statistics.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_permutation.h>
+#include <gsl/gsl_sort_vector.h>
+#include <gsl/gsl_histogram.h>
+
+#include "../cylutils/cyl.h"
+
+extern void ROIprint(VImage zmap,VImage metric,VImage roi,int,VString filename);
+extern VImage Convert2Repn(VImage src,VImage dest,VRepnKind repn);
+  
+int main (int argc, char **argv)
+{
+  static VString  roi_filename="";
+  static VString  metric_filename="";
+  static VString  txt_filename="";
+  static VShort   nbins = 15;
+  static VOptionDescRec options[] = {
+    {"metric", VStringRepn,1,(VPointer) &metric_filename,VRequiredOpt,NULL,"metric image"},
+    {"out", VStringRepn,1,(VPointer) &txt_filename,VRequiredOpt,NULL,"Output txt file"},
+    {"roi", VStringRepn,1,(VPointer) &roi_filename,VRequiredOpt,NULL,"ROI image"},
+    {"nbins", VShortRepn,1,(VPointer) &nbins,VOptionalOpt,NULL,"Number of bins"},
+  };
+  VString in_filename=NULL;
+  char *prg=GetLipsiaName("vcylarim_roi");
+  fprintf (stderr, " %s\n", prg);
+
+  
+  /* parse command line */
+  VParseFilterCmdX(VNumber(options),options,argc,argv,&in_filename,NULL);
+
+ 
+  /* read zmap  */
+  VImage zmap=NULL;
+  VAttrList zlist = VReadAttrList(in_filename,0L,TRUE,FALSE);
+  VImage ztmp = VReadImage(zlist);
+  if (ztmp == NULL) VError(" no zmap found");
+  zmap = Convert2Repn(ztmp,zmap,VFloatRepn);
+  VDestroyImage(ztmp);
+
+  
+  /* metric image */
+  VImage metric=NULL;
+  VAttrList xlist = VReadAttrList(metric_filename,0L,TRUE,FALSE);
+  if (xlist == NULL) VError(" error reading file %s",metric_filename);
+  VImage tmp = VReadImage(xlist);
+  if (tmp == NULL) VError(" err reading %s",metric_filename);
+  if (VImageNPixels(tmp) != VImageNPixels(zmap))
+    VError(" inconsistent image dimensions: metric vs zmap");
+  if (VPixelRepn(tmp) != VFloatRepn && VPixelRepn(tmp) != VDoubleRepn)
+    VError(" metric image should be 4-byte or 8-byte float");
+  metric = Convert2Repn(tmp,metric,VFloatRepn);
+  VDestroyImage(tmp);
+
+ 
+  /* ROI image */
+  VAttrList mlist = VReadAttrList(roi_filename,0L,TRUE,FALSE);
+  if (mlist == NULL) VError(" error reading file %s",roi_filename);
+  VImage mtmp = VReadImage(mlist);
+  if (mtmp == NULL) VError(" err reading %s",roi_filename);
+  if (VImageNPixels(mtmp) != VImageNPixels(zmap))
+    VError(" inconsistent image dimensions: ROI vs zmap");
+  VImage roi = Convert2Repn(mtmp,NULL,VBitRepn);
+  VDestroyImage(mtmp);
+
+  
+  /* main */
+  ROIprint(zmap,metric,roi,(int)nbins,txt_filename);
+
+
+  fprintf (stderr, "%s: done.\n", argv[0]);
+  return 0;
+}
